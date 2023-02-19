76c691c88d865882c9eb09ec7e7da056e54ef38f
SonaliN84 <sonali.nilekar@gmail.com>
2023-02-19 11:29:17 +0530

    task6 sharpener

---
+++ task6/script.js
@@ -0,0 +1,52 @@
+// var input=document.querySelector("input");
+// input.value="hello world";
+
+// var submit=document.querySelector('input[type="submit"]')
+// submit.value="SEND"
+
+// var item=document.querySelector(".list-group-item")
+// item.style.color="red"
+
+// var lastitem=document.querySelector(".list-group-item:last-child")
+// // console.log(lastitem)
+// lastitem.style.color="blue"
+
+// var secondItem=document.querySelector('.list-group-item:nth-child(2)')
+// secondItem.style.color="pink"
+
+// //QuerySelectorAll
+
+// var titles=document.querySelectorAll('.title')
+// console.log(titles)
+// titles[0].textContent="hello"
+
+// var odd=document.querySelectorAll("li:nth-child(odd)")
+// for(var i=0;i<odd.length;i++){
+//    odd[i].style.background="aqua"
+// }
+// var even=document.querySelectorAll("li:nth-child(even)")
+// for(var i=0;i<even.length;i++){
+//    even[i].style.background="pink"
+// }
+
+
+///Deliverable 1
+// var item=document.querySelector('.list-group-item:nth-child(2)')
+// item.style.backgroundColor="green";
+
+// var item3=document.querySelector('.list-group-item:nth-child(3)')
+// item3.style.display="none"
+
+
+//deliverable 2
+var item=document.querySelectorAll("li")
+item[1].style.color="green"
+
+var odd=document.querySelectorAll("li:nth-child(odd)")
+for(var i=0;i<odd.length;i++){
+   odd[i].style.background="green"
+}
+
+
+
+
