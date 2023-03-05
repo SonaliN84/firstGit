aed27f5b4a1990afc229cbf47b3df6e5859a9a59
SonaliN84 <sonali.nilekar@gmail.com>
2023-03-05 19:51:36 +0530

    Frontend React Task

---
+++ frontend-react/src/App.js
@@ -0,0 +1,13 @@
+import ExpenseItems from './component/ExpenseItem';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1 id="h1">Let's get started</h1>
+    <ExpenseItems></ExpenseItems>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend-react/src/component/ExpenseItem.js
@@ -0,0 +1,17 @@
+
+import './component.css'
+function ExpenseItems(){
+   
+   return ( 
+    <div class="h3">
+       <h2>Expense Items</h2>
+       <div class="div">
+       <div>Food Rs 10</div>
+      <div> Petrol Rs 100</div>
+      <div> Movies Rs 200 </div>
+      </div>                         
+       </div>
+   
+   );
+}
+export default ExpenseItems;
